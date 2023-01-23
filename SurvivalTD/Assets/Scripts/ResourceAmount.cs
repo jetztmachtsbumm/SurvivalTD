5757b071bd2a18b678fea21cec6e55c2d5581e9c
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2023-01-23 16:55:02 +0100

    Resource system

---
+++ SurvivalTD/Assets/Scripts/ResourceAmount.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceAmount
+{
+
+    public ResourceTypeSO resourceType;
+    public int amount;
+
+}
